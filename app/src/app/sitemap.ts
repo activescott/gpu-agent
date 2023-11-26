50a122944fd9edd31589b04bb5123ab3be2f0784
Scott Willeke <scott@willeke.com>
2023-11-25 16:07:20 -0800

    fix: smarter sitemap changefreq for shop pages

--- app/src/app/sitemap.ts
+++ app/src/app/sitemap.ts
@@ -5,6 +5,8 @@ import { IterableElement } from "type-fest"
 
 /* eslint-disable import/no-unused-modules */
 
+type SitemapItem = IterableElement<MetadataRoute.Sitemap>
+
 const sitemapEntries = [...sitemapJson.data]
 
 // https://developers.google.com/search/docs/crawling-indexing/sitemaps/build-sitemap
@@ -13,16 +15,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const now = new Date()
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
   const domain_url = `https://${ISOMORPHIC_CONFIG.NEXT_PUBLIC_DOMAIN()}`
-  type SitemapItem = IterableElement<MetadataRoute.Sitemap>
+
   const items = [
     ...sitemapEntries.map((item) => {
       return {
         url: `${domain_url}${item.path}`,
-        changeFrequency: "monthly",
+        changeFrequency: chooseChangeFrequency(item),
         priority: 0.8,
         lastModified: item.lastModified,
       } satisfies SitemapItem
     }),
+    // then add the root/home page:
     {
       url: `${domain_url}/`,
       changeFrequency: "daily",
@@ -32,4 +35,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
   ]
 
   return items
+}
+
+interface SitemapJsonItem {
+  path: string
+  title: string
+  lastModified: string
+}
+
+function chooseChangeFrequency(
+  item: SitemapJsonItem,
+): SitemapItem["changeFrequency"] {
+  const path = item.path
+  if (path.startsWith("/ml/shop")) {
+    return "daily"
+  }
+  if (path.startsWith("/policy")) {
+    return "yearly"
+  }
+  return "monthly"
 }

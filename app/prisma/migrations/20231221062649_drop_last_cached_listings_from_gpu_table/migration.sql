b32c5b09952ef13ca3e4a9d856c77e2ed965441f
Scott Willeke <scott@willeke.com>
2023-12-20 22:28:47 -0800

    chore: drop lastCachedListings from gpu table

---
+++ app/prisma/migrations/20231221062649_drop_last_cached_listings_from_gpu_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lastCachedListings` on the `gpu` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "gpu" DROP COLUMN "lastCachedListings";

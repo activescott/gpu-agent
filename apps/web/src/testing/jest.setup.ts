4d68bec751dca0f6c8a30f74e4982174da87e14b
Scott Willeke <scott@willeke.com>
2025-01-03 23:33:24 -0800

    chore: fixes lint issues with new test setup

--- apps/web/src/testing/jest.setup.ts
+++ apps/web/src/testing/jest.setup.ts
@@ -1,9 +1,13 @@
-import dotenv from "dotenv"
+import { config } from "dotenv"
 import path from "path"
+import { fileURLToPath } from "url"
 
+// disable because this is used by jest.setup.ts
+// eslint-disable-next-line import/no-unused-modules
 export default function loadEnv() {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url))
   const envPath = path.resolve(__dirname, "../../.env.local")
-  const result = dotenv.config({
+  const result = config({
     path: envPath,
   })
   if (result.error) {
